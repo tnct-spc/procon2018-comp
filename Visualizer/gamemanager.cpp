a2742d079f09c6c573a2b8b14fbb68ce6a3f0a3e
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-04-04 18:45:27 +0900

    [add]changeTurnを完全に実装した

--- Visualizer/gamemanager.cpp
+++ Visualizer/gamemanager.cpp
@@ -29,10 +29,13 @@ bool GameManager::agentAct(int turn, int agent, int type, int x_pos, int y_pos){
     //クッソ長い例外処理
     if(type && (abs(x_pos) > 1 || abs(y_pos) > 1 || (!x_pos && !y_pos) ||
         agent_pos.first + x_pos < 0 || agent_pos.first + x_pos >= grid_size.first ||
-        agent_pos.second + y_pos < 0 || agent_pos.second + y_pos >= grid_size.second))
+        agent_pos.second + y_pos < 0 || agent_pos.second + y_pos >= grid_size.second ||
+        (type == 1 && field->getState(agent_pos.first + x_pos, agent_pos.second + y_pos).first == (turn ? 2 : 1)) ||
+        (type == 2 && field->getState(agent_pos.first + x_pos, agent_pos.second + y_pos).first != (turn ? 1 : 2))
+         ))
         return false;
 
-    act_stack.at(turn).at(agent) = std::make_pair(type, std::make_pair(x_pos, y_pos));
+    act_stack.at(turn).at(agent) = std::make_pair(type, std::make_pair(agent_pos.first + x_pos, agent_pos.second + y_pos));
 
     for(int turn_flag = 0; turn_flag < 2; ++turn_flag)
         for(int agent_num = 0; agent_num < 2; ++agent_num)
@@ -46,16 +49,37 @@ bool GameManager::agentAct(int turn, int agent, int type, int x_pos, int y_pos){
 void GameManager::changeTurn(){
 
     std::map<std::pair<int,int>,std::vector<std::pair<int,int>>> dest_map;
+    std::map<std::pair<int,int>,std::vector<std::pair<int,int>>> tile_map;
 
     for(int turn_flag = 0; turn_flag < 2; ++turn_flag)
-        for(int agent_num = 0; agent_num < 2; ++agent_num)
+        for(int agent_num = 0; agent_num < 2; ++agent_num){
             if(act_stack.at(turn_flag).at(agent_num).first == 1)
                 dest_map[act_stack.at(turn_flag).at(agent_num).second].push_back( std::make_pair(turn_flag, agent_num) );
+            else if(act_stack.at(turn_flag).at(agent_num).first == 2){
+                tile_map[act_stack.at(turn_flag).at(agent_num).second].push_back( std::make_pair(turn_flag, agent_num) );
+            }
+        }
 
     for(auto elements : dest_map){
         if(elements.second.size() > 1)
             continue;
         field->setAgent(elements.second.at(0).first, elements.second.at(0).second, elements.first.first, elements.first.first);
+        field->setState(elements.first.first, elements.first.second, elements.second.at(0).first);
+    }
+
+    for(auto elements : tile_map){
+        bool state_flag = true;
+        if(elements.second.size() > 1)
+            continue;
+
+        for(int turn_flag = 0; turn_flag < 2; ++turn_flag)
+            for(int agent_num = 0; agent_num < 2; ++agent_num)
+                if(field->getAgent(turn_flag, agent_num) == elements.first){
+                    state_flag = false;
+                    break;
+                }
+        if(state_flag)
+            field->setState(elements.first.first, elements.first.second, 0);
     }
 
 
