67d87f2ee50c8e09a45b993ac02407077f383307
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-04-14 15:07:46 +0900

    [update]agentact部分

--- Solver/simplemontecarlo/simplemontecarlo.h
+++ Solver/simplemontecarlo/simplemontecarlo.h
@@ -13,6 +13,9 @@ class SimpleMonteCalro : public AlgorithmWrapper
     using AlgorithmWrapper::AlgorithmWrapper;
 public:
     const std::pair<std::tuple<int,int,int>, std::tuple<int,int,int>> agentAct(int side);
+private:
+    //試行回数 適当に変えてね
+    unsigned int trial = 20;
 
 };
 
