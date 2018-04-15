38eb87d347f1fc286750b79b1b428d92f8d4c448
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-04-15 21:30:30 +0900

    [update]manager経由でfieldを取ってきてcanputにfieldを渡していい感じに

--- Solver/montecarlotreesearch/searchnode.h
+++ Solver/montecarlotreesearch/searchnode.h
@@ -2,6 +2,7 @@
 #define SEARCHNODE_H
 
 #include <vector>
+#include "field.h"
 
 class MonteCarloTreeSearch;
 
@@ -9,7 +10,7 @@ class SearchNode
 {
 public:
 
-    SearchNode(MonteCarloTreeSearch *tree, SearchNode *parent, std::vector<std::pair<int,int>> agents, int x_inp, int y_inp);
+    SearchNode(MonteCarloTreeSearch *tree, SearchNode *parent, std::vector<std::pair<int,int>> agents, int side, int x_inp, int y_inp);
 
     bool canMove(int agent_1_move, int agent_2_move);
 
@@ -17,15 +18,22 @@ public:
     static constexpr double ucb_value = 1.41421356;
     static int size_x;
     static int size_y;
+    const std::vector<int> x_list = { 0, 0, 1, 1, 1, -1, -1, -1, 0};
+    const std::vector<int> y_list = { 1, -1, 1, 0, -1, 1, 0, -1, 0};
+
 
     unsigned int win_count = 0;
     unsigned int try_count = 0;
+    int side;
+
 
     MonteCarloTreeSearch *tree;
     SearchNode *parent;
 
     std::vector<std::pair<int,int>> agents;
     std::vector<SearchNode*> childs;
+
+    const procon::Field& field;
 
 };
 
