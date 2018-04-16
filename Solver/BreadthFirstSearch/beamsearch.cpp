af3e8e40eb7f0a688ce707510a77ff2c28ccba0e
hamuhei4869 <s17031@tokyo.kosen-ac.jp>
2018-04-16 18:52:07 +0900

    [add]評価関数が一応でけた

--- Solver/BreadthFirstSearch/beamsearch.cpp
+++ Solver/BreadthFirstSearch/beamsearch.cpp
@@ -10,10 +10,19 @@ int beamsearch::Evaluation_Field(procon::Field field,int turn){
         agent2=Agents.at(1).at(1);
     }
     std::pair<int,int> size = field.getSize();
-    int Eva = 0;
+    int Eva_manhattan = 0;
+    std::vector<std::vector<int>> value = field.getValue();
     for(int a = 0;a < size.first;a++){
         for(int b =0;b < size.second;b++){
-
+            Eva_manhattan += (value.at(a).at(b)+16)/(abs(agent1.first-a)+abs(agent1.second-b));
+            Eva_manhattan += (value.at(a).at(b)+16)/(abs(agent2.first-a)+abs(agent2.second-b));
         }
+    }
+    return Eva_manhattan;
+}
+int beamsearch::agentAct(int side){
+    std::queue<std::pair<int,procon::Field>> beam;
+    for(int a = 0;a < beam_range;a++){
+
     }
 }
--- Solver/BreadthFirstSearch/beamsearch.h
+++ Solver/BreadthFirstSearch/beamsearch.h
@@ -4,13 +4,13 @@
 #include "gamemanager.h"
 #include "algorithmwrapper.h"
 
-class beamsearch
+class beamsearch : public AlgorithmWrapper
 {
 
 public:
     beamsearch();
     int beam_range;
-    int Evaluation_Field(procon::Field field);
+    int Evaluation_Field(procon::Field field,int turn);
     const std::pair<std::tuple<int,int,int>, std::tuple<int,int,int>> agentAct(int side);
     std::pair<int,int> agent1,agent2;
 };
