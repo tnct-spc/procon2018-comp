430901849e63b383d60f9fee289309e2c027182c
hamuhei4869 <s17031@tokyo.kosen-ac.jp>
2018-04-17 18:29:02 +0900

    [add]歯医者でコーデイングするオタクのモノマネをしている

--- Solver/BreadthFirstSearch/beamsearch.h
+++ Solver/BreadthFirstSearch/beamsearch.h
@@ -12,9 +12,11 @@ class beamsearch : public AlgorithmWrapper
 public:
     beamsearch();
     int beam_range;
+    int beam_turn;
     int Evaluation_Field(procon::Field field,int turn);
     std::pair<std::tuple<int,int,int>, std::tuple<int,int,int>> agentAct(int side);
     std::pair<int,int> agent1,agent2;
+
 };
 
 #endif // BEAMSEARCH_H
