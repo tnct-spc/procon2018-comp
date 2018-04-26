1237ea2af2ddff5fe2867157a256f4b21a1fa972
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-04-26 18:20:15 +0900

    [add]関数

--- Solver/geneticalgo/geneticalgo.cpp
+++ Solver/geneticalgo/geneticalgo.cpp
@@ -1,6 +1,16 @@
 #include "geneticalgo.h"
 
+GeneticAlgo::GeneticAlgo(std::shared_ptr<GameManager> manager_ptr) :
+    AlgorithmWrapper(manager_ptr)
+{
+
+}
+
 const std::pair<std::tuple<int,int,int>, std::tuple<int,int,int>> GeneticAlgo::agentAct(int side)
 {
 
+}
+
+void GeneticAlgo::setAgentData(GeneticAgent inp){
+    agent_data = inp;
 }
--- Solver/geneticalgo/geneticalgo.h
+++ Solver/geneticalgo/geneticalgo.h
@@ -2,13 +2,21 @@
 #define GENETICALGO_H
 
 #include "algorithmwrapper.h"
+#include "geneticagent.h"
 
 class GeneticAlgo : public AlgorithmWrapper
 {
-    using AlgorithmWrapper::AlgorithmWrapper;
+    // using AlgorithmWrapper::AlgorithmWrapper;
 public:
-    GeneticAlgo();
+
+    GeneticAlgo(std::shared_ptr<GameManager> manager_ptr);
+    void setAgentData(GeneticAgent inp);
     const std::pair<std::tuple<int,int,int>, std::tuple<int,int,int>> agentAct(int side);
+
+private:
+    GeneticAgent agent_data;
+
+    //こっちはパラメータの通りに動くやつ
 };
 
 #endif // GENETICALGO_H
