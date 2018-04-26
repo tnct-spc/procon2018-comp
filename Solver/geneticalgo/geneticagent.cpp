584408069b2aaa567e61255cd13f1bfb0c1973ed
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-04-26 22:47:55 +0900

    [add]agent側の変数とコンストラクタ

--- Solver/geneticalgo/geneticagent.cpp
+++ Solver/geneticalgo/geneticagent.cpp
@@ -3,4 +3,14 @@
 GeneticAgent::GeneticAgent()
 {
 
+    std::random_device rnd;
+    std::mt19937 mt(rnd());
+
+    std::uniform_real_distribution<> rand_double(0.0, 1.0);
+
+    rand = rand_double(mt);
+    minus = rand_double(mt);
+    next_p = rand_double(mt);
+    region = rand_double(mt);
+    removal = rand_double(mt);
 }
--- Solver/geneticalgo/geneticagent.h
+++ Solver/geneticalgo/geneticagent.h
@@ -1,6 +1,7 @@
 #ifndef GENETICAGENT_H
 #define GENETICAGENT_H
 
+#include <random>
 
 class GeneticAgent
 {
@@ -8,6 +9,12 @@ public:
     GeneticAgent();
 
     //パラメータを持ってて実質構造体
+
+    double rand = 0.5; //乱択度合い(高いほど乱択)
+    double minus = 0.5; //マイナスの回避度(高いほど避ける)
+    double next_p = 0.5; //次の手での得点の優先度(高いほど優先)
+    double region = 0.5; //領域ポイントの優先度(高いほど優先)
+    double removal = 0.5; //タイル除去の優先度(高いほど優先)
     
 
 
