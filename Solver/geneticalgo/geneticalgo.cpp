e64fda8593f172ebfc375107f0b1bf2d898356cb
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-04-27 00:18:39 +0900

    [add]tile_valueをえいえい

--- Solver/geneticalgo/geneticalgo.cpp
+++ Solver/geneticalgo/geneticalgo.cpp
@@ -29,6 +29,30 @@ const std::pair<std::tuple<int,int,int>, std::tuple<int,int,int>> GeneticAlgo::a
 
     std::vector<double> eval(can_put_pattern.size(), 0);
 
+
+    //とりあえずここで評価していく
+    auto tile_value = [&](std::pair<int,int> pos){
+        double value;
+        std::pair<int,int> state = field.getState(pos.first, pos.second);
+
+        if(state.first == (side == 0 ? 2 : 1)){//除去の場合
+
+            if(state.second >= 0)value = state.second * (agent_data.removal * 2.0);//除去特有の重み付け
+            else value = state.second * ( 0.5 / agent_data.removal );
+
+        }else{
+
+            value = state.second;
+        }
+
+        if(state.second < 0)
+            value += state.second * ( agent_data.minus * 3.0);//マイナスの時は評価を減らす
+
+        value += state.second * ( agent_data.next_p * 2.0);//得点に合わせて加算
+
+        return value;
+    };
+
     for(unsigned int index = 0; index < can_put_pattern.size(); ++index){
 
         int count = can_put_pattern.at(index);
@@ -42,6 +66,9 @@ const std::pair<std::tuple<int,int,int>, std::tuple<int,int,int>> GeneticAlgo::a
         pos.at(1) = field.getAgent(side, 1);
         pos.at(1).first += x_list.at(count % 9);
         pos.at(1).second += y_list.at(count % 9);
+
+        double value1 = tile_value(pos.at(0));
+        double value_2 = tile_value(pos.at(1));
 
     }
 
