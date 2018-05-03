0e0adb01e4d09b5104045340b9a932b4b339fb1e
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-05-03 11:10:41 +0900

    [add]時間計測とシミュレーション

--- Solver/simplemontecarlo/montecarlowithalgo.cpp
+++ Solver/simplemontecarlo/montecarlowithalgo.cpp
@@ -12,9 +12,6 @@ const std::pair<std::tuple<int,int,int>, std::tuple<int,int,int>> MontecarloWith
 
     const procon::Field& field = manager->getField();
 
-    win_count.resize(81, 0);
-    try_count.resize(81, 0);
-
     auto retRandom = [&](int st,int en){ //[st,en]
         std::uniform_int_distribution<> rand(st, en);
 
@@ -55,6 +52,56 @@ const std::pair<std::tuple<int,int,int>, std::tuple<int,int,int>> MontecarloWith
 
         bool win = mgr->simulationGenetic(*side_1, *side_2, 0);
 
+        ++try_sum;
+
+        return win;
     };
+
+    auto ucb = [&](int index){return 1.0 * win_count.at(index) / try_count.at(index) + ucb_val * sqrt(( 2.0 * std::log(try_sum)) / try_count.at(index));};
+
+    std::vector<int> can_move_list;
+    for(int count = 0; count < 81; ++count)
+        if(manager->canPut(0, count / 9, count % 9)){
+
+            can_move_list.push_back(count);
+            try_count.push_back(1);
+            win_count.push_back( playout(count) );
+        }
+
+
+    auto simulation = [&]{
+
+        double ucb_max = -1;
+        int max_index = 0;
+        //最大要素を確認する
+        for(unsigned int index = 0; index < can_move_list.size(); ++index){
+            double ucb_index = ucb(can_move_list.at(index) );
+
+            if(ucb_max < ucb_index){
+                ucb_max = ucb_index;
+                max_index = index;
+            }
+        }
+
+        //ucbの最大値でシミュレーション
+        bool win = playout(can_move_list.at(max_index));
+
+        ++try_count.at(max_index);
+        win_count.at(max_index) += win;
+    };
+
+    clock_t start_time = clock();
+
+    int count = can_move_list.size() - 1;
+    while(++count){
+
+        //毎回の計測はちょっと遅くなるのでダメ
+        if(count % 10 == 0){
+            clock_t now_time = clock(); //時間計測
+            if(try_time < static_cast<double>(now_time - start_time) )
+                break;
+        }
+
+    }
 
 }
