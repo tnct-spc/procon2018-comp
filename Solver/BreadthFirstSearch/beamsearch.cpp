0b049503af1914385cd98a63f2491b5a26e83db4
hamuhei4869 <s17031@tokyo.kosen-ac.jp>
2018-05-05 16:07:49 +0900

    [add]コメントを追加、varをメンバでなく関数内に持たせた

--- Solver/BreadthFirstSearch/beamsearch.cpp
+++ Solver/BreadthFirstSearch/beamsearch.cpp
@@ -1,5 +1,5 @@
 #include "beamsearch.h"
-typedef std::pair<double,std::tuple<procon::Field,int,int>> origin;
+typedef std::pair<double,std::tuple<procon::Field,int,int>> point_field_pos;
 
 beamsearch::beamsearch(std::shared_ptr<GameManager> manager_ptr, const GeneticAgent& agent) :
     AlgorithmWrapper(manager_ptr),
@@ -11,6 +11,8 @@ beamsearch::beamsearch(std::shared_ptr<GameManager> manager_ptr, const GeneticAg
 double beamsearch::Evaluation_Field(procon::Field field,int side){
 
     std::vector<double> agent_value = agent_data.getData();
+
+    const double var1=1,var2=0,var3=0,var4=0,var5=0;
 
     agent_value.at(0) = var1;
     agent_value.at(1) = var2;
@@ -90,7 +92,7 @@ const std::pair<std::tuple<int,int,int>,std::tuple<int,int,int>> beamsearch::age
 
     std::pair<int,int> grid_size = field.getSize();
 
-    std::vector<std::pair<int,int>> age1,age2;
+    std::vector<std::pair<int,int>> age1,age2; //それぞれのagentが行動できる方向
 
     age1.push_back(std::make_pair(0,1));
     age1.push_back(std::make_pair(0,-1));
@@ -104,50 +106,52 @@ const std::pair<std::tuple<int,int,int>,std::tuple<int,int,int>> beamsearch::age
     age2 = age1;
     std::vector<std::vector<std::pair<double,int>>> Eva_stack(8 ,std::vector<std::pair<double,int>>(8 , std::make_pair(0,0))); //age1,age2の組み合わせの得点/回数
     std::vector<std::vector<std::pair<int,int>>> ways(8,std::vector<std::pair<int,int>>(8 , std::make_pair(0,0)));
-    std::priority_queue<origin,std::vector<origin>,
-            std::function<bool(origin,origin)>>
-            beam([](origin a,origin b) -> bool {if(a.first<b.first){return true;}else{return false;}});
-    int RemainingTurn = manager->getFinalTurn()-manager->getTurnCount();
-    beam_turn = std::min(beam_turn,RemainingTurn);
-    for(int turn = 0;turn < beam_turn;turn++){
-        if(turn == 0){
+    //agent1,agen2が使う能力の種類age1,age2と関連性があり、それぞれのindexをways.at(a).at(b)と打つことでagent1が向きage1.at(a)をagent2が向きage2.at(b)を持ったときpair(agent1が移動or破壊,agent2が移動or破壊)が返ってくる
+    std::priority_queue<point_field_pos,std::vector<point_field_pos>,
+            std::function<bool(point_field_pos,point_field_pos)>>
+            beam([](point_field_pos a,point_field_pos b) -> bool {if(a.first<b.first){return true;}else{return false;}});//priority_queueで.firstつまり評価値を比べて高い順に先頭から並べる
+    int RemainingTurn = manager->getFinalTurn()-manager->getTurnCount(); //残りターン数
+    beam_turn = std::min(beam_turn,RemainingTurn); //残りターン数を超えるところまで探索しても無意味で無価値なので、そこを調整
+    for(int turn = 0;turn < beam_turn;turn++){ //探索ターン数分探索
+        if(turn == 0){                         //最初のターンかどうか(現在のfieldから派生しているかどうかで分岐)
 
-            for(int a = 0;a < 8;a++){
+            for(int a = 0;a < 8;a++){          //agent1の動き方8通り
 
-                if(age1.at(a).first+agent1.first < 0)continue;
+                if(age1.at(a).first+agent1.first < 0)continue;                 //agent1が盤面の外側に対して何かしようとしてないか例外処理
                 if(age1.at(a).first+agent1.first >= grid_size.first)continue;
                 if(age1.at(a).second+agent1.second < 0)continue;
-                if(age1.at(a).second+agent1.second >= grid_size.second)continue;
+                if(age1.at(a).second+agent1.second >= grid_size.second)continue; //ここまで
 
                 for(int b = 0;b < 8;b++){
-                    if(age2.at(b).first+agent2.first < 0)continue;
+                    if(age2.at(b).first+agent2.first < 0)continue;            //agent2版
                     if(age2.at(b).first+agent2.first >= grid_size.first)continue;
                     if(age2.at(b).second+agent2.second < 0)continue;
-                    if(age2.at(b).second+agent2.second >= grid_size.second)continue;
+                    if(age2.at(b).second+agent2.second >= grid_size.second)continue; //ここまで
 
-                    std::vector<std::vector<std::pair<int,std::pair<int,int>>>> pos;
+                    std::vector<std::vector<std::pair<int,std::pair<int,int>>>> pos; //敵味方四人のagentのpair<破壊or移動,pair<それをする座標x,y>>
 
-                    std::vector<std::pair<int,std::pair<int,int>>> ins_pos;
+                    std::vector<std::pair<int,std::pair<int,int>>> ins_pos;          //posに代入するための変数
 
-                    int way1=1,way2=1;
+                    int way1=1,way2=1;                                               //agent1,agent2がそれぞれなにをするのか格納
 
                    if(side==0){
-                    way1 = (field.getState(agent1.first+age1.at(a).first,agent1.second+age1.at(a).second).first == ( side ? 1 : 2 ) ? 2 : 1 );
+                    //赤色サイド
+                    way1 = (field.getState(agent1.first+age1.at(a).first,agent1.second+age1.at(a).second).first == ( side ? 1 : 2 ) ? 2 : 1 );//はいはい決めます
 
-                    way2 = (field.getState(agent2.first+age2.at(b).first,agent2.second+age2.at(b).second).first == ( side ? 1 : 2 ) ? 2 : 1 );
+                    way2 = (field.getState(agent2.first+age2.at(b).first,agent2.second+age2.at(b).second).first == ( side ? 1 : 2 ) ? 2 : 1 );//決めます
 
-                    ins_pos.push_back(std::make_pair(way1,std::make_pair(agent1.first+age1.at(a).first,agent1.second+age1.at(a).second)));
-                    ins_pos.push_back(std::make_pair(way2,std::make_pair(agent2.first+age2.at(b).first,agent2.second+age2.at(b).second)));
+                    ins_pos.push_back(std::make_pair(way1,std::make_pair(agent1.first+age1.at(a).first,agent1.second+age1.at(a).second)));//とりまins_posに
+                    ins_pos.push_back(std::make_pair(way2,std::make_pair(agent2.first+age2.at(b).first,agent2.second+age2.at(b).second)));//同乗
 
-                    pos.push_back(ins_pos);
+                    pos.push_back(ins_pos);//posに追加
 
-                    ins_pos.clear();
-                    ins_pos.push_back(std::make_pair(0,std::make_pair(agent1.first,agent1.second)));
-                    ins_pos.push_back(std::make_pair(0,std::make_pair(agent2.first,agent2.second)));
-                    pos.push_back(ins_pos);
+                    ins_pos.clear(); //ins_posを消す
+                    ins_pos.push_back(std::make_pair(0,std::make_pair(agent1.first,agent1.second)));//敵のagentは動かない前提で探索しています(よくない)(wayを0にすれば勝手に動かないので座標はデタラメ)
+                    ins_pos.push_back(std::make_pair(0,std::make_pair(agent2.first,agent2.second)));//同上
+                    pos.push_back(ins_pos);//posに追加
 
                    }else{
-
+                       //青色サイド大体赤色とおんなじことやってる
                        ins_pos.push_back(std::make_pair(0,std::make_pair(agent1.first,agent1.second)));
                        ins_pos.push_back(std::make_pair(0,std::make_pair(agent2.first,agent2.second)));
                        pos.push_back(ins_pos);
@@ -163,30 +167,28 @@ const std::pair<std::tuple<int,int,int>,std::tuple<int,int,int>> beamsearch::age
                        pos.push_back(ins_pos);
                    }
 
-                    procon::Field ins_field = agentmove(field,pos);
+                    procon::Field ins_field = agentmove(field,pos);//fieldとそれぞれのagentの動きを引数にとって、行動後のfieldを返す関数
 
-                    double Eva = Evaluation_Field(ins_field,side);
+                    double Eva = Evaluation_Field(ins_field,side);//移動後のfieldを評価
 
-                    ways.at(a).at(b).first=way1;
-                    ways.at(a).at(b).second=way2;
+                    ways.at(a).at(b).first=way1;                  //waysに移動方法を格納
+                    ways.at(a).at(b).second=way2;                 //同上
 
-                    beam.push(std::make_pair(Eva,std::make_tuple(ins_field,a,b)));
-                    Eva_stack.at(a).at(b).first+=Eva;
-                    Eva_stack.at(a).at(b).second++;
+                    beam.push(std::make_pair(Eva,std::make_tuple(ins_field,a,b)));//beamにpush!
+                    Eva_stack.at(a).at(b).first+=Eva;//手の得点の総和を格納
+                    Eva_stack.at(a).at(b).second++;//得点を足した回数
                     count++;
                 }
             }
         }else{
-            int beam_size = beam.size();
-            std::priority_queue<origin,std::vector<origin>,
-                   std::function<bool(origin,origin)>>
-                    beam_ins([](origin a,origin b) -> bool{return a.first < b.first;});
-         //   cout<<beam_size<<endl;
-            for(int range = 0;range < std::min(beam_range,beam_size);range++){
-            procon::Field Neo_ins_field = std::get<0>(beam.top().second);
+            int beam_size = beam.size();//beamのsizeを格納
+            std::priority_queue<point_field_pos,std::vector<point_field_pos>,
+                   std::function<bool(point_field_pos,point_field_pos)>>
+                    beam_ins([](point_field_pos a,point_field_pos b) -> bool{return a.first < b.first;});//beamの代替品
+            for(int range = 0;range < std::min(beam_range,beam_size);range++){                           //beamから高い順にmin(beamの大きさ,beam幅)個考えて回す
+            procon::Field Neo_ins_field = std::get<0>(beam.top().second);                                //ここから一ターン目とだいたいおんなじことやってます
             agent1 = Neo_ins_field.getAgent(side,0);
             agent2 = Neo_ins_field.getAgent(side,1);
-         //   std::cout<<beam.top().first<<std::endl;
             for(int a = 0;a < 8;a++){
 
                 if(age1.at(a).first+agent1.first < 0)continue;
@@ -247,22 +249,22 @@ const std::pair<std::tuple<int,int,int>,std::tuple<int,int,int>> beamsearch::age
                     count++;
                     }
                 }
-            beam.pop();
+            beam.pop();//探索済みの値を削除
             }
-            beam = beam_ins;
+            beam = beam_ins;//beamにbeam_insを代入
         }
     }
 
 
-    std::pair<std::tuple<int,int,int>,std::tuple<int,int,int>> ans;
-    double most_Eva = -1000000;
+    std::pair<std::tuple<int,int,int>,std::tuple<int,int,int>> ans;//最終的なagentactの意志
+    double most_Eva = -1000000;                                    //最も高い答えを格納
     for(int a = 0;a < 8;a++){
-        for(int b = 0;b < 8;b++){
-            if(Eva_stack.at(a).at(b).second==0)continue;
-            //std::cout<<Eva_stack.at(a).at(b).first<<std::endl;
-            if(most_Eva<=1.0000*Eva_stack.at(a).at(b).first/Eva_stack.at(a).at(b).second){
-                most_Eva=Eva_stack.at(a).at(b).first/Eva_stack.at(a).at(b).second;
+        for(int b = 0;b < 8;b++){                                  //全ての手8*8から派生しうる手の評価の平均を比べる(これが高いと、この手を打ったときに平均的に高い点数が望まれる(いい手))
+            if(Eva_stack.at(a).at(b).second==0)continue;           //派生していない、つまり盤面外に対する操作なので考えない
+            if(most_Eva<=1.0000*Eva_stack.at(a).at(b).first/Eva_stack.at(a).at(b).second){//平均を取った値がmost_Evaより大きければ
+                most_Eva=Eva_stack.at(a).at(b).first/Eva_stack.at(a).at(b).second;        //その値を格納
                 ans= std::make_pair(std::make_tuple(ways.at(a).at(b).first,age1.at(a).first,age1.at(a).second),std::make_tuple(ways.at(a).at(b).second,age2.at(b).first,age2.at(b).second));
+                //ansに答えを代入
             }
         }
     }
--- Solver/BreadthFirstSearch/beamsearch.h
+++ Solver/BreadthFirstSearch/beamsearch.h
@@ -22,7 +22,6 @@ public:
     double Evaluation_Field(procon::Field field,int side);
     const std::pair<std::tuple<int,int,int>, std::tuple<int,int,int>> agentAct(int side);
     std::pair<int,int> agent1,agent2;
-    const double var1=1,var2=0,var3=0,var4=0,var5=0;
     //マスのポイント、乱択要素、味方との距離、マイナスのポイントをいかに踏んでいないか、敵との距離
 private:
     procon::Field agentmove(procon::Field field,std::vector<std::vector<std::pair<int,std::pair<int,int>>>> pos);
