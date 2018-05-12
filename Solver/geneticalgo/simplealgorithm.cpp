0d0f30a3385c322466a79a01e79d44a5ffbd8d92
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-05-12 11:31:31 +0900

    [add]倍率とか定数

--- Solver/geneticalgo/simplealgorithm.cpp
+++ Solver/geneticalgo/simplealgorithm.cpp
@@ -18,8 +18,6 @@ double SimpleAlgorithm::evaluateMove(int side, std::pair<int, int> move){
 
     procon::Field& field = manager->getField();
 
-    const std::vector<double>& data = agent_data.getData();
-
     std::vector<int> move_vec(2); //ここの実装くそ
     move_vec.at(0) = move.first;
     move_vec.at(1) = move.second;
@@ -35,6 +33,31 @@ double SimpleAlgorithm::evaluateMove(int side, std::pair<int, int> move){
         move_pos.at(index).second += y_list.at(move_vec.at(index));
         state.at(index) = field.getState(move_pos.at(index).first, move_pos.at(index).second);
     }
+
+    const std::vector<double>& data = agent_data.getData();
+
+    /*
+
+      除去,タイル無しに移動,自タイルに移動
+      0点除去,+除去,-除去,+移動,-移動,0点移動,自タイル移動
+
+    */
+
+    double const_minus_move = -1.0 * data.at(0) * 100 + 30;//マイナスを踏んだ時の定数
+    double const_plus_move = data.at(1) * 100 - 30;
+
+    double per_minus_move = -1.0 * data.at(2) * 20 + 6;//マイナスを踏んだ時の得点にかかる倍率
+    double per_plus_move = data.at(3) * 20 - 6;
+
+    double const_back_move = -1.0 * data.at(4) * 100 + 30;//自分の陣地に戻った時の定数
+
+    double const_no_move = -1.0 * data.at(5) * 100 + 30;//動かなかった時の定数
+
+    double const_delete_move = data.at(6) * 100 - 30;//タイル除去にかかる定数
+    double per_delete_move = data.at(7) * 20 - 6;//タイル除去にかかる倍率
+
+    double per_region = data.at(8) * 20 - 6;//領域ポイントにかかる倍率
+
 
 
 }
