6e66394cd2da437b8191f93b265c39273eced1de
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-05-13 19:43:26 +0900

    [update]あたい

--- Simulator/simulatedannealing.h
+++ Simulator/simulatedannealing.h
@@ -41,7 +41,7 @@ private:
     const std::pair<int,int> buttle_rand = std::make_pair(60, 7);
     //直接対決の(重み,一回辺りの対戦数)
     //総得点が等しくなるのを防ぐために重みを小数にしている
-    const std::pair<double,int> buttle_direct = std::make_pair(9.5, 9);
+    const std::pair<double,int> buttle_direct = std::make_pair(2.5, 9);
 
     //開始時の温度(温度は線形で変化させる)
     const double start_temp = 0.125 * (buttle_rand.first + buttle_direct.first);
