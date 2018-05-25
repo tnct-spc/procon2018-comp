178c9ecf33e8c136d060a3364cd588105f720b70
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-05-25 12:40:39 +0900

    [add]入力

--- Test/main.cpp
+++ Test/main.cpp
@@ -4,5 +4,24 @@
 int main()
 {
 
+    std::cout << "select mode" << std::endl;
+    std::cout << "0 : TestAgentParam" << std::endl;
+
+
+    int test_number = -1;
+
+    while(1){
+
+        std::cout << "algo number : ";
+        std::cin >> test_number;
+
+        if(test_number == 0)
+            break;
+
+        std::cout << "incorrect" << std::endl;
+    }
+
+
+
     return 0;
 }
