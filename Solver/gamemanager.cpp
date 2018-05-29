6cb2fdfd04b3fd2a1cde3cbd9688ea4d8c9de1f4
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-05-29 09:34:54 +0900

    [update]出力

--- Solver/gamemanager.cpp
+++ Solver/gamemanager.cpp
@@ -148,8 +148,10 @@ void GameManager::startSimulation(QString my_algo, QString opponent_algo) {
             red_point = field->getPoints(0);
             blue_point = field->getPoints(1);
 
+            /*
             std::cout<<"赤の素の得点は"<<red_point.first<<"点で、領域ポイントは"<<red_point.second<<"点です"<<std::endl;
             std::cout<<"青の素の得点は"<<blue_point.first<<"点で、領域ポイントは"<<blue_point.second<<"点です"<<std::endl;
+            */
 
             field_vec.push_back(std::make_shared<procon::Field>(*field));
 
