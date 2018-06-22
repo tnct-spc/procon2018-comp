5ab639878dd0370150c33e732390f29655d3f832
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-06-22 18:10:52 +0900

    [add]盤面の自動生成

--- Test/testevaluatespeed.cpp
+++ Test/testevaluatespeed.cpp
@@ -2,9 +2,38 @@
 
 TestEvaluateSpeed::TestEvaluateSpeed()
 {
-
+    mt = std::mt19937(rnd());
 }
 
 void TestEvaluateSpeed::run(){
+
+    auto rand = [&](int max){
+        std::uniform_int_distribution<> dist(0, max);
+        return dist(mt);
+    };
+
+
+    // 盤面の自動生成
+    procon::Field field(8 + rand(4), 8 + rand(4), 16, -16);
+
+    std::pair<int,int> size = field.getSize();
+
+    for(int x = 0; x < size.first; ++x)
+        for(int y = 0; y < size.second; ++y)
+            field.setState(x, y, rand(2));
+
+    std::set<std::pair<int,int>> agent_pos;
+
+    for(int side = 0; side < 2; ++side)
+        for(int agent = 0; agent < 2; ++agent){
+
+            std::pair<int,int> new_pos = std::make_pair(rand(field.getSize().first - 1), rand(field.getSize().second - 1));
+            while(agent_pos.count(new_pos))
+                new_pos = std::make_pair(rand(field.getSize().first - 1), rand(field.getSize().second - 1));
+
+            field.setAgent(side, agent, new_pos.first, new_pos.second);
+            field.setState(new_pos.first, new_pos.second, side + 1);
+            agent_pos.insert(new_pos);
+        }
 
 }
--- Test/testevaluatespeed.h
+++ Test/testevaluatespeed.h
@@ -1,12 +1,17 @@
 #ifndef TESTEVALUATESPEED_H
 #define TESTEVALUATESPEED_H
 
+#include "field.h"
 
 class TestEvaluateSpeed
 {
 public:
     TestEvaluateSpeed();
     void run();
+
+private:
+    std::random_device rnd;
+    std::mt19937 mt;
 };
 
 #endif // TESTEVALUATESPEED_H
