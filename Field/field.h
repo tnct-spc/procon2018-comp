cc6c73a310abd652dc14e9cfde58aa1e520b392c
hamuhei4869 <s17031@tokyo.kosen-ac.jp>
2018-06-24 20:20:54 +0900

    [add]枝切り

--- Field/field.h
+++ Field/field.h
@@ -26,7 +26,10 @@ public:
 
     std::vector<std::vector<bool>> getRegion(int side);
 
-    std::pair<int,int> getPoints(int side, bool update_flag = true); //sideの<踏んだマス,領域点>を返す
+    std::pair<int,int> getPoints(int side, bool flag = true);
+    std::pair<int,int> getPoints(int side, bool flag, std::pair<int, std::pair<int, int>> pos);     //sideの<踏んだマス,領域点>を返す,引数posには<影響を与える色<座標>>を投げて
+    std::pair<int,int> getPoints(int side, bool flag, std::vector<std::pair<int, std::pair<int, int>>> pos_vec);
+
     void setPoints(int side, std::pair<int,int> value);
 
     bool isPlaced(const unsigned int x, const unsigned int y);
