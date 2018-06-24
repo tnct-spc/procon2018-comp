05c0dc8db8d00a319ec9df7ab58591396bfdeea2
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-06-24 20:59:33 +0900

    [add]thread一部

--- Solver/doubleagent/algorithms/simplemcforduble.cpp
+++ Solver/doubleagent/algorithms/simplemcforduble.cpp
@@ -3,21 +3,25 @@
 SimpleMCForDuble::SimpleMCForDuble(std::shared_ptr<GameManager> manager, std::vector<std::shared_ptr<AgentWrapper>> agents, int side) :
     manager(manager),
     side(side),
+    cpu_num(std::thread::hardware_concurrency()),
     field(manager->getField()),
     agents(agents)
 {
+    mt = std::mt19937(rnd());
 }
 
-void SimpleMCForDuble::changeTurn(){
+void SimpleMCForDuble::changeTurn(GameManager* manager_ptr){
 
     std::vector<std::vector<std::pair<double,std::tuple<int,int,int>>>> can_move_list;
+
+    procon::Field& ptr_field = manager_ptr->getField();
 
     for(int index = 0; index < 2; ++index)
         can_move_list.at(index).push_back(std::make_pair(0.0, std::make_tuple(0, 0, 0)));
 
     auto agent_move = [&](int agent){
 
-        std::pair<int,int> agent_pos = field.getAgent(side, agent);
+        std::pair<int,int> agent_pos = ptr_field.getAgent(side, agent);
 
         auto evaluate = [&](int count, bool is_delete){
 
@@ -34,10 +38,10 @@ void SimpleMCForDuble::changeTurn(){
             new_pos.first += x_list.at(count);
             new_pos.second += y_list.at(count);
 
-            if(new_pos.first < 0 || new_pos.first >= field.getSize().first || new_pos.second < 0 || new_pos.second >= field.getSize().second)
+            if(new_pos.first < 0 || new_pos.first >= ptr_field.getSize().first || new_pos.second < 0 || new_pos.second >= ptr_field.getSize().second)
                 continue;
 
-            int state = field.getState(new_pos.first, new_pos.second).first;
+            int state = ptr_field.getState(new_pos.first, new_pos.second).first;
 
             if(state != (side ? 1 : 2))
                 evaluate(count, false);
@@ -53,5 +57,46 @@ void SimpleMCForDuble::changeTurn(){
 }
 
 std::pair<std::tuple<int,int,int>,std::tuple<int,int,int>> SimpleMCForDuble::calcMove(){
+    // ここがagentActから呼び出される
+
+    auto rand = [&](int max){
+        std::uniform_int_distribution<> dist(0, max);
+        return dist(mt);
+    };
+
+    //answer[行動] = (勝数,試行回数)
+    std::map<std::tuple<int,int,int>, std::pair<int,int>> answer;
+
+    std::vector<std::future<std::map<std::tuple<int,int,int>,std::pair<int,int>>>> threads;
+    for(int count = 0; count < cpu_num; ++count)
+        threads.push_back(std::async([&]{
+
+            clock_t start = clock();
+
+            // 返却する値
+            std::map<std::tuple<int,int,int>,std::pair<int,int>> return_map;
+
+            // 一定時間が経つまで
+            while((double)(clock() - start) / CLOCKS_PER_SEC < calc_time){
+
+                //ここに書く
+                int turn_count = 60 + rand(60);
+                GameManager* manager_ptr = new GameManager(8 + rand(4), 8 + rand(4), false, turn_count);
+
+                for(int count = 0; count < turn_count; ++count)
+                    changeTurn(manager_ptr);
+            }
+
+            return return_map;
+        }));
+
+    for(auto &th : threads){
+        std::map<std::tuple<int,int,int>,std::pair<int,int>> return_val = th.get();
+
+        for(auto value : return_val){
+            answer[value.first].first += value.second.first;
+            answer[value.first].second += value.second.second;
+        }
+    }
 
 }
--- Solver/doubleagent/algorithms/simplemcforduble.h
+++ Solver/doubleagent/algorithms/simplemcforduble.h
@@ -1,6 +1,8 @@
 #ifndef SIMPLEMCFORDUBLE_H
 #define SIMPLEMCFORDUBLE_H
 
+#include <future>
+
 #include "gamemanager.h"
 #include "doubleagent/agents/agentwrapper.h"
 
@@ -14,17 +16,24 @@ private:
     std::shared_ptr<GameManager> manager;
     std::vector<std::shared_ptr<AgentWrapper>> agents;
 
-    void changeTurn();
+    void changeTurn(GameManager* manager_ptr);
 
     // 探索候補に含める最小値(これ以下は探索しない)
     const double minus_bound = -5000;
     // 重みにかける累乗の(大きいほど評価の高い値を優先的に探索する)
     const double value_weight = 1.0;
 
+    // 計算に書ける時間(sec)
+    const double calc_time = 2.0;
+
     std::vector<int> x_list = {1, 1, 1, 0,  0, -1, -1, -1, 0};
     std::vector<int> y_list = {-1, 0, 1, -1, 1, -1, 0, 1, 0};
 
+    std::random_device rnd;
+    std::mt19937 mt;
+
     int side;
+    int cpu_num;
 
     // ここでFieldのコピーをしてしまう (プレイアウト回数)だけコピーをしてしまうのは仕方ないです
     procon::Field field;
