5c64f0218201dc885e5510ed3ba22e95d512329c
GODofMEGANE <s18195@tokyo.kosen-ac.jp>
2018-06-28 16:59:10 +0900

    [add]convertの新しいクラス

---
+++ QRcode/qrconverterfield.cpp
@@ -0,0 +1,6 @@
+#include "qrconverterfield.h"
+
+QrConverterField::QrConverterField()
+{
+
+}
---
+++ QRcode/qrconverterfield.h
@@ -0,0 +1,11 @@
+#ifndef QRCONVERTERFIELD_H
+#define QRCONVERTERFIELD_H
+
+
+class QrConverterField
+{
+public:
+    QrConverterField();
+};
+
+#endif // QRCONVERTERFIELD_H
