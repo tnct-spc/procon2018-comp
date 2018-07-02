5596808a17d36a574eff1a7db67c1271408d8771
GODofMEGANE <s18195@tokyo.kosen-ac.jp>
2018-07-02 17:41:38 +0900

    なくしました

--- QRcode/qrconverterfield.cpp
+++ QRcode/qrconverterfield.cpp
@@ -1,11 +1,9 @@
 #include "qrconverterfield.h"
-#include <bits/stdc++.h>
-using namespace std;
 
 // vector<vector<int>> vec の二次元配列に点数
 // vector<int> A (2) , vector<int> B (2) それぞれにエージェントの初期位置
 
-int ctoi(string s,int& i) {
+int ctoi(std::string s,int& i) {
     int keta = 0;
     for(int j = 0;true;j++){
         if(s.at(i+j) == '-' || s.at(i+j) == '0' || s.at(i+j) == '1' || s.at(i+j) == '2' || s.at(i+j) == '3' || s.at(i+j) == '4' || s.at(i+j) == '5' || s.at(i+j) == '6' || s.at(i+j) == '7' || s.at(i+j) == '8' || s.at(i+j) == '9'){
@@ -19,7 +17,6 @@ int ctoi(string s,int& i) {
     bool minus = false;
     for(int j = 0;j < keta;j++){
         count *= 10;
-        cout << s.at(i) << endl;
         switch (s.at(i+j)) {
             case '-': minus = true;break;
             case '0': count += 0;break;
@@ -39,18 +36,17 @@ int ctoi(string s,int& i) {
         count *= -1;
     }
     i += keta;
-    cout << "final number is " << count << endl;
     return count;
 }
 
 QrConverterField::QrConverterField()
 {
-    string s = read_only_data;
+    std::string s = read_only_data;
     int stage = 0;
-    queue<int> HW;
+    std::queue<int> HW;
     int H, W;
     int i = 0;
-    vector<vector<int>> vec;
+    std::vector<std::vector<int>> vec;
     for(i = 0;stage != 2;i++){
             if(s.at(i) == '-' || s.at(i) == '0' || s.at(i) == '1' || s.at(i) == '2' || s.at(i) == '3' || s.at(i) == '4' || s.at(i) == '5' || s.at(i) == '6' || s.at(i) == '7' || s.at(i) == '8' || s.at(i) == '9'){
                 HW.push(ctoi(s,i));
@@ -62,7 +58,7 @@ QrConverterField::QrConverterField()
     HW.pop();
     W = HW.front();
     int brock = 0;
-    vec = vector<vector<int>>(H, vector<int>(W, 0));
+    vec = std::vector<std::vector<int>>(H, std::vector<int>(W, 0));
     for(stage = 0;stage <= H;i++){
         if(s.at(i) == ':'){
             stage++;
@@ -75,8 +71,8 @@ QrConverterField::QrConverterField()
                 continue;
             }
     }
-    vector<int> A (2);
-    vector<int> B (2);
+    std::vector<int> A (2);
+    std::vector<int> B (2);
     int b = 0;
     for(int a = 0;a < 2;i++){
         if(s.at(i) == ':'){
