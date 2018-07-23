bd42c336fa9c02bb73a8a638e8e2a407113c1b99
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-07-23 12:56:16 +0900

    [update]文字列の初期読み込み

--- Test/testconvertfield.cpp
+++ Test/testconvertfield.cpp
@@ -7,8 +7,11 @@ TestConvertField::TestConvertField()
 
 void TestConvertField::run(){
     std::string input;
-    std::cout << "please input" << std::endl;
+    /*
+    std::cout << "please input : ";
     std::cin >> input;
+    */
+    input = "8 11:-2 1 0 1 2 0 2 1 0 1 -2:1 3 2 -2 0 1 0 -2 2 3 1:1 3 2 1 0 -2 0 1 2 3 1:2 1 1 2 2 3 2 2 1 1 2: 2 1 1 2 2 3 2 2 1 1 2: 1 3 2 1 0 -2 0 1 2 3 1: 1 3 2 -2 0 1 0 -2 2 3 1: -2 1 0 1 2 0 2 1 0 1 -2:2 2:7 10: ";
 
     QrConverterField StrToField;
     procon::Field field = StrToField.ConvertCsvToField(input);
