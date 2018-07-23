8df877241ac3b3b056cd8959e372b68fb0a65dea
reinagaya <rei.nagaya.00@gmail.com>
2018-07-24 00:22:54 +0900

    [add]UIデザイン第1案

--- Visualizer/operator.cpp
+++ Visualizer/operator.cpp
@@ -1,11 +1,14 @@
 #include "operator.h"
 #include "ui_operator.h"
 
-Operator::Operator(QWidget *parent) :
+Operator::Operator(procon::Field& inp_field, QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::Operator)
+    ui(new Ui::Operator),
+    field(inp_field)
 {
     ui->setupUi(this);
+
+    field = inp_field;
 }
 
 Operator::~Operator()
--- Visualizer/operator.h
+++ Visualizer/operator.h
@@ -1,6 +1,8 @@
 #ifndef OPERATOR_H
 #define OPERATOR_H
 
+#include "field.h"
+#include "visualizer.h"
 #include <QWidget>
 
 namespace Ui {
@@ -12,11 +14,13 @@ class Operator : public QWidget
     Q_OBJECT
 
 public:
-    explicit Operator(QWidget *parent = 0);
+    explicit Operator(procon::Field& inp_field, QWidget *parent = 0);
     ~Operator();
 
 private:
     Ui::Operator *ui;
+
+    procon::Field field;
 };
 
 #endif // OPERATOR_H
