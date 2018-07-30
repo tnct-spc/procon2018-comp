1bd9fea4b9388fce5e4158d313799cff162bcbbe
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-07-30 17:21:38 +0900

    [update]read_binary

--- Python/readbinary.py
+++ Python/readbinary.py
@@ -1,6 +1,6 @@
 import const
 
-def make_field(f):
+def unpack_field(f, field_list):
 
     field = {}
 
@@ -14,18 +14,34 @@ def make_field(f):
     field['state'] = [[0] * siz[1]] * siz[0]
     field['value'] = [[0] * siz[1]] * siz[0]
 
-    for x in range(siz[0]):
-        for y in range(size[1]):
+    for x in range(field['siz'][0]):
+        for y in range(field['siz'][1]):
+            dat = f.read(1)
+            field['state'][x][y] = dat >> 6
+            field['value'][x][y] = dat & ((1 << 6) - 1)
+    
+    field['now_turn'] = f.read(1)
+    field['final_turn'] = f.read(1)
+
+    field['agent'] = [[[0] * 2] * 2] * 2
+
+    for side in range(2):
+        for agent in range(2):
+            dat = f.read(1)
+            field['agent'][side][agent][0] = dat >> 4
+            field['agent'][side][agent][1] = dat & ((1 << 4) - 1)
+    
+    field_list.append(field)
 
     return True
 
 def read_binary():
 
-    file = open('csv_path', 'rb')
+    file = open(const.csv_path, 'rb')
 
     move_list = []
 
-    while make_field(f, move_list):
+    while unpack_field(file, move_list):
         pass
 
 
