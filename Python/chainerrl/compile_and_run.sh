a6161428457f55761c1d11472e51aad468d00f3c
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-08-06 15:16:45 +0900

    [add]shfile

--- Python/chainerrl/compile_and_run.sh
+++ Python/chainerrl/compile_and_run.sh
@@ -1,3 +1,3 @@
-g++ -shared -fPIC -I../../Field -L../../Field -lpython3.6m -I/usr/include/python3.6m -lboost_numpy3 -lboost_python3 -o communication.so ../../Field/field.cpp ../communication.cpp
+g++ -shared -fPIC -I../../Field -L../../Field -I../../GameManager -L../../GameManager -lpython3.6m -I/usr/include/python3.6m -lboost_numpy3 -lboost_python3 -o communication.so ../../Field/field.cpp ../../GameManager/GameManager.cpp ../communication.cpp
 # LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/shibh308/workspace/procon2018/procon2018-comp/Field" 
 python main.py
