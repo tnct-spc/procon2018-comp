bca9bffdb2d7fe499ca205bbc51ee088584eb52d
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-08-06 15:53:15 +0900

    [add]sh

--- Python/chainerrl/compile_and_run.sh
+++ Python/chainerrl/compile_and_run.sh
@@ -1,3 +1,3 @@
-g++ -shared -fPIC -I../../Field -L../../Field -lpython3.6m -I/usr/include/python3.6m -lboost_numpy3 -lboost_python3 -o communication.so ../../Visualizer/visualizer.cpp ../../Solver/gamemanager.cpp ../../Field/field.cpp ../communication.cpp
+g++ -shared -fPIC -lqt -I../../Field -L../../Field -I../../Visualizer -L../../Visualizer -I../../GameManager -I/usr/include/qt -I/usr/include/qt/QtGui -I/usr/include/qt/QtCore -I/usr/include/qt/QtWidgets -L../../GameManager -lQtCore -lQtGui -lQtWidgets -lpython3.6m -I/usr/include/python3.6m -lboost_numpy3 -lboost_python3 -o communication.so ../../Field/field.cpp ../../Visualizer/visualizer.cpp ../../Solver/gamemanager.cpp ../communication.cpp
 # LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/shibh308/workspace/procon2018/procon2018-comp/Field" 
 python main.py
