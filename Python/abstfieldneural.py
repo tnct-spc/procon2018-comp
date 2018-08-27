d4eb6b415732431c2ed5cf05a096d7230568fc08
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-08-27 15:52:08 +0900

    [add]abstfieldneural

--- Python/abstfieldneural.py
+++ Python/abstfieldneural.py
@@ -18,19 +18,19 @@ def abst_field_neural():
     random.shuffle(data)
 
     inp_data = np.zeros((len(data), 30), dtype=np.float32)
-    out_data = np.empty((len(data), 1), dtype=np.float32)
+    out_data = np.empty((len(data), 3), dtype=np.float32)
 
-    for index in range(len(data)):
+    for index in range(min(100000,len(data))):
         for i in range(30):
             inp_data[index][i] = data[index][i]
-        out_data[index][0] = 0 if data[index][30] == data[index][31] else 1 if data[index][30] > data[index][31] else 2
+        out_data[index][0 if data[index][30] == data[index][31] else 1 if data[index][30] > data[index][31] else 2] = 1.0
 
     train_size = int((1.0 - const.test_data_per) * len(data))
 
     train_data = chainer.datasets.TupleDataset(inp_data[:train_size], out_data[:train_size])
     test_data = chainer.datasets.TupleDataset(inp_data[train_size:], out_data[train_size:])
 
-    network.calc_neural([600, 500, 1], train_data, test_data, const.result_path + '_abst_field/', '_abst_field', int(min(const.max_train, train_size)), int(min(const.max_test, int(const.test_data_per * len(data)))), const.epoch)
+    network.calc_neural([30, 30, 3], train_data, test_data, const.result_path + '_abst_field/', '_abst_field', int(min(const.max_train, train_size)), int(min(const.max_test, int(const.test_data_per * len(data)))), const.epoch)
 
 
 abst_field_neural()
