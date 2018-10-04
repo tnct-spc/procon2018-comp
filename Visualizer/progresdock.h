74091663e462737b5d647b7c808f6a768f1d2856
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-10-04 12:18:24 +0900

    [update]Dockへの値設定等

--- Visualizer/progresdock.h
+++ Visualizer/progresdock.h
@@ -20,9 +20,11 @@ public:
 
     void addVisuAnswer(procon::Field& field);
     void addMinumuVisu(std::pair<int,int> size, std::vector<std::list<std::pair<int,int>>> route, std::vector<std::vector<std::vector<int>>> color);
+    void setValuesToBack(const std::vector<std::vector<double>>& values);
 
 private:
     Ui::ProgresDock *ui;
+    MinimumVisualizer* dock_back;
     unsigned int field_count = 0;
 //    std::vector<procon::Field> fields;
 };
