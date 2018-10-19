624499355e40d82d68b2d70762d527172a27c4cb
shibh308 <s17097@tokyo.kosen-ac.jp>
2018-10-19 09:54:10 +0900

    [fix]apply_conflict上で係数を逆にしていた問題

--- Solver/warshallfloydalgorithm.cpp
+++ Solver/warshallfloydalgorithm.cpp
@@ -26,14 +26,14 @@ const std::pair<std::tuple<int,int,int>, std::tuple<int,int,int>> WarshallFloydA
             std::pair<int,int> next_move_pos = pos_pair.second;
 
             if(next_move_pos == field.getAgent(side ^ 1, 0) || next_move_pos == field.getAgent(side ^ 1, 1))
-                pos_pair.first *= conflict_def_per;
+                pos_pair.first *= conflict_atk_per;
 
             if(field.getState(next_move_pos.first, next_move_pos.second).first == (side ? 1 : 2) &&
                 ((std::abs(next_move_pos.first - field.getAgent(side ^ 1, 0).first) <= 1 &&
                 std::abs(next_move_pos.second - field.getAgent(side ^ 1, 0).second) <= 1) ||
                 (std::abs(next_move_pos.first - field.getAgent(side ^ 1, 1).first) <= 1 &&
                 std::abs(next_move_pos.second - field.getAgent(side ^ 1, 1).second) <= 1)))
-                pos_pair.first *= conflict_atk_per;
+                pos_pair.first *= conflict_def_per;
         }
     };
 
